1778c17ba69dab0a2c1a84ea9bfaa75f53dcf7cf
Anderson (Akame!) <andersonrobertopetry@gmail.com>
2022-07-05 19:45:31 -0300

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template 
-app = Flask(__name__) #instanciando classe flask
+from flask import Flask, render_template
+app = Flask(__name__) #Instanciando classe Flask.
 
-#endereço do site
-@app.route('/') #rota principal
-@app.route('/index') #rota alternativa
+#Endereço do site.
+@app.route('/') #Rota principal.
+@app.route('/index') #Rota alternativa.
 def index():
-  #return "Olá cidadão(ã)"
-  nome="Anderson Petry"
-  dados={"Profissão":"Developer", "Cidade":"Santa Rosa"} #variavel list
-  return render_template('index.html',nome=nome, dados=dados)
+    #return "Hello, world!"
+    nome='Anderson' #Variável.
+    dados={"idade":"20 anos", "cidade":"Santa Rosa"} #Variável.
+    return render_template('index.html',nome=nome, dados=dados)
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+@app.route('/produtos')
+def produtos():
+    return render_template('produtos.html')      
+
+
+
+
+
+
+
+app.run()
